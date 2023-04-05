b7d5672ca3bb7c01922de511335d981357d94945
quinjenn <jquintal_@hotmail.com>
2023-04-05 16:28:13 -0700

    git add admin dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
    http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
+    @products_inventory = Product.all
+    @categories_inventory = Category.all
   end
 end
