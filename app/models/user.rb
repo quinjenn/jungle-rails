23341ff04ca4ccd7507b4a0625b42ce81f6c22f3
quinjenn <jquintal_@hotmail.com>
2023-04-11 16:51:00 -0700

    new auth class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false } 
   validates :password, presence: true, length: { minimum: 6 }, confirmation: true
   validates :password_confirmation, presence: true
+
+   def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email.strip.downcase)
+    user && user.authenticate(password) ? user : nil
+  end
 end
 
 
